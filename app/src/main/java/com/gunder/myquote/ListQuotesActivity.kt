4c6cd3ee09ff919c0fa65386855c02d9524be5ce
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-10-09 12:47:35 +0700

    design layout dan item layout recycelerView

--- app/src/main/java/com/gunder/myquote/ListQuotesActivity.kt
+++ app/src/main/java/com/gunder/myquote/ListQuotesActivity.kt
@@ -1,4 +1,15 @@
 package com.gunder.myquote
 
-class ListQuotesActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gunder.myquote.databinding.ActivityListQuotesBinding
+import com.gunder.myquote.databinding.ActivityMainBinding
+
+class ListQuotesActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityListQuotesBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityListQuotesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
 }
