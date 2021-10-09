9b43e6f3e53971197dc03c9d1668bec734131e6e
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-10-09 13:37:51 +0700

    setting recycelerView

--- app/src/main/java/com/gunder/myquote/QuotesAdapter.kt
+++ app/src/main/java/com/gunder/myquote/QuotesAdapter.kt
@@ -1,4 +1,26 @@
 package com.gunder.myquote
 
-class QuotesAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class QuotesAdapter(private val listView: ArrayList<String>): RecyclerView.Adapter<QuotesAdapter.ViewHolder>(){
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val tvItem: TextView = view.findViewById(R.id.tvItem)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_quote,parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+       viewHolder.tvItem.text = listView[position]
+    }
+
+    override fun getItemCount(): Int {
+        return listView.size
+    }
 }
